942c7aa56d9ce19fa420ca6ae2a98637b8eb60db
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-10-19 00:39:56 +0530

    message

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBUgs882siQ3lbFpgjZZ7FM1Y5bzshH4Uk")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="tell me computer network"
